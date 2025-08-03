a715aeef2b599bb7e215618c5ad04175f45c1c5a
heynoxcodes <heynox1000@gmail.com>
2025-08-03 23:05:04 +0200

    Update layout.js

--- createxyz-project/src/app/layout.js
+++ createxyz-project/src/app/layout.js
@@ -11,16 +11,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Font Awesome */}
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-          integrity="sha512-yTUZSwU8iUJqEgaZfdZGckb6+evRWZL7Sjl2Nz8Xl6B+vd8r2koxqYbhGPa9QMy2k1f0SENv6IH4y8Go9mB+Qw=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-      </head>
+      <html lang="en">
+  <head>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      integrity="sha512-TbdR6+sIWB0Bvph/0UQK2EJbOZa7Kx2DyQx0I3Zb1WRFzH1g+2Xq3+zZcLOO8ULP4axI6pPaP0ZZIPAPHiHE4g=="
+      crossOrigin="anonymous"
+      referrerPolicy="no-referrer"
+    />
+  </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
