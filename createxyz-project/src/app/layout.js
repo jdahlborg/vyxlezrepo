406f507d5321e1e5f223e6f359b70f190e115d69
heynoxcodes <heynox1000@gmail.com>
2025-08-04 03:39:56 +0200

    Update layout.js

--- createxyz-project/src/app/layout.js
+++ createxyz-project/src/app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'noxability',
-  description: 'Made by nox',
+  title: 'Vyxlez Portfolio',
+  description: 'Vyxlez portofolio website',
 }
 
 export default function RootLayout({ children }) {
